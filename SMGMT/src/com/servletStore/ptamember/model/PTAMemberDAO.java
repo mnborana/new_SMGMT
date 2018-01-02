0dc823b9294ca7116faff00f8601492f7f2deabd
SANDEEP BATULE <SANDEEP BATULE@sandeepbatule>
2018-01-02 11:17:18 +0530

    smgmt updated

--- SMGMT/src/com/servletStore/ptamember/model/PTAMemberDAO.java
+++ SMGMT/src/com/servletStore/ptamember/model/PTAMemberDAO.java
@@ -6,5 +6,7 @@ public interface PTAMemberDAO {
 	
 	public List getStandardList(PTAMemberPojo pojo);
 	public List getStudentNameList(PTAMemberPojo pojo);
-
+	public List getParentDetails(PTAMemberPojo pojo);
+	public List getMembers(String id);
+	public List getStudentParentDetails(PTAMemberPojo pojo);
 }
