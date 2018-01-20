ba56c7a69757cb4ed02b28f4d6a4ac1170105f7e
SANDEEP BATULE <SANDEEP BATULE@sandeepbatule>
2018-01-20 21:27:58 +0530

    sandeep batule updated

--- SMGMT/src/com/servletStore/fees/assignStdWiseFees/controller/AssignStdWiseFees.java
+++ SMGMT/src/com/servletStore/fees/assignStdWiseFees/controller/AssignStdWiseFees.java
@@ -2,13 +2,13 @@ package com.servletStore.fees.assignStdWiseFees.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/AssignStdWiseFees")
+
 public class AssignStdWiseFees extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
