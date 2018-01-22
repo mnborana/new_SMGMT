c5482266f2d6c2ef5cb144fdb41ec40398076246
VSoftware <VSoftware@VSoftware-PC>
2018-01-22 11:34:11 +0530

    a

--- SMGMT/src/com/servletStore/fees/assignStdWiseFees/controller/AssignStdWiseFees.java
+++ SMGMT/src/com/servletStore/fees/assignStdWiseFees/controller/AssignStdWiseFees.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/AssignStdWiseFees")
 public class AssignStdWiseFees extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
